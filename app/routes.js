627aa3075c101da65ab13d32d3b0bbef80f055b0
GGiles <greggiles@gmail.com>
2017-05-14 22:38:19 -0400

    removing unused stuff. fixing user update

--- app/routes.js
+++ app/routes.js
@@ -106,8 +106,10 @@ module.exports = function(app, passport) {
 
         // SIGNUP =================================
         // show the signup form
-        app.get('/signup', function(req, res) {
-            res.render('signup.ejs', { message: req.flash('signupMessage') });
+        app.get('/signup', isLoggedIn, function(req, res) {
+            res.render('signup.ejs', {
+                user : req.user
+            });
         });
 
         // process the signup form
@@ -149,43 +151,6 @@ module.exports = function(app, passport) {
 // used to unlink accounts. for social accounts, just remove the token
 // for local account, remove email and password
 // user account will stay active in case they want to reconnect in the future
-
-    // local -----------------------------------
-    app.get('/unlink/local', isLoggedIn, function(req, res) {
-        var user            = req.user;
-        user.local.email    = undefined;
-        user.local.password = undefined;
-        user.save(function(err) {
-            res.redirect('/profile');
-        });
-    });
-
-    // facebook -------------------------------
-    app.get('/unlink/facebook', isLoggedIn, function(req, res) {
-        var user            = req.user;
-        user.facebook.token = undefined;
-        user.save(function(err) {
-            res.redirect('/profile');
-        });
-    });
-
-    // twitter --------------------------------
-    app.get('/unlink/twitter', isLoggedIn, function(req, res) {
-        var user           = req.user;
-        user.twitter.token = undefined;
-        user.save(function(err) {
-            res.redirect('/profile');
-        });
-    });
-
-    // google ---------------------------------
-    app.get('/unlink/google', isLoggedIn, function(req, res) {
-        var user          = req.user;
-        user.google.token = undefined;
-        user.save(function(err) {
-            res.redirect('/profile');
-        });
-    });
 
     // strava ---------------------------------
     app.get('/unlink/strava', isLoggedIn, function(req, res) {
